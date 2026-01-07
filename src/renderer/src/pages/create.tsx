a06f2a4c0a453eb33c1849ef18222bfd0e221b47
valdeirgsilva <valdeirgabriels@gmail.com>
2026-01-07 11:14:46 -0300

    Página cadastro de clientes

--- src/renderer/src/pages/create.tsx
+++ src/renderer/src/pages/create.tsx
@@ -1,22 +1,100 @@
+import { FormEvent, useRef } from 'react'
+
 export function Create() {
-  async function handleAddCustomer() {
-    const doc = {
-      name: 'Lucas Silva',
-      email: 'lucas@teste.com',
-      phone: '21999999999',
-      address: 'Rua X, Centro',
-      role: 'Frontend',
-      status: true
+  const nameRef = useRef<HTMLInputElement | null>(null)
+  const emailRef = useRef<HTMLInputElement | null>(null)
+  const addressRef = useRef<HTMLInputElement | null>(null)
+  const phoneRef = useRef<HTMLInputElement | null>(null)
+  const roleRef = useRef<HTMLInputElement | null>(null)
+
+  async function handleAddCustomer(e: FormEvent) {
+    e.preventDefault()
+
+    const name = nameRef.current?.value
+    const address = addressRef.current?.value
+    const email = emailRef.current?.value
+    const phone = phoneRef.current?.value
+    const role = roleRef.current?.value
+
+    if (!name || !address || !email || !phone || !role) {
+      return
     }
 
-    const response = await window.api.addCustomer(doc)
-    console.log(response)
+    console.log({
+      name,
+      address,
+      email,
+      phone,
+      role
+    })
+
+    // const response = await window.api.addCustomer(doc)
+    // console.log(response)
   }
   return (
-    <div>
-      <h1>Página Novo cliente!!!</h1>
+    <div className="flex-1 flex flex-col py-12 px-10 gap-8 overflow-y-auto">
+      <section className="flex flex-1 flex-col items-center">
+        <h1 className="text-white text-xl lg:text-3xl font-semibold">Cadastrar novo cliente</h1>
 
-      <button onClick={handleAddCustomer}>CADASTRAR</button>
+        <form className="w-full max-w-96 mt-4" onSubmit={handleAddCustomer}>
+          <div className="mb-2">
+            <label className="text-lg">Nome:</label>
+            <input
+              type="text"
+              placeholder="Digite nome do cliente..."
+              className="w-full h-9 rounded text-black px-2"
+              ref={nameRef}
+            />
+          </div>
+
+          <div className="mb-2">
+            <label className="text-lg">Endereço:</label>
+            <input
+              type="text"
+              placeholder="Digite endereço completo..."
+              className="w-full h-9 rounded text-black px-2"
+              ref={addressRef}
+            />
+          </div>
+
+          <div className="mb-2">
+            <label className="text-lg">Email:</label>
+            <input
+              type="text"
+              placeholder="Digite o email..."
+              className="w-full h-9 rounded text-black px-2"
+              ref={emailRef}
+            />
+          </div>
+
+          <div className="mb-2">
+            <label className="text-lg">Cargo:</label>
+            <input
+              type="text"
+              placeholder="Digite o cargo..."
+              className="w-full h-9 rounded text-black px-2"
+              ref={roleRef}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="text-lg">Telefone:</label>
+            <input
+              type="text"
+              placeholder="Digite o telefone..."
+              className="w-full h-9 rounded text-black px-2"
+              ref={phoneRef}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-blue-500 rounded flex items-center justify-center w-full h-9"
+          >
+            Cadastrar
+          </button>
+        </form>
+      </section>
     </div>
   )
 }
